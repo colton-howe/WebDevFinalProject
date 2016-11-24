dc1201b2e442b1fb92afa684b94d55af215e05c6
RSCArt <ryancarew@live.ca>
2016-11-23 19:13:15 -0500

    Added Page
    Added the Registration page, linked to index page.

---
+++ js/register.js
@@ -0,0 +1,50 @@
+$(function() {
+$("#user :input").keyup(function(){
+var user = $("#user >:input").val();
+    
+    if(user.length < 6) {
+        $("#user").addClass("has-error");
+        $("#user").removeClass("has-success");
+        
+    } else {
+        $("#user").addClass("has-success");
+        $("#user").removeClass("has-error");
+    }
+});
+});
+
+$(function() {
+$("#pass-check :input").keyup(function(){
+    var psw = $("#psw >:input").val();
+    var confirmpsw = $("#confirm-psw >:input").val();
+    if(psw.length < 6) {
+        $("#psw").addClass("has-error");
+        $("#psw").removeClass("has-success");
+        
+    } else {
+        $("#psw").addClass("has-success");
+        $("#psw").removeClass("has-error");
+    }
+    if(confirmpsw == psw){
+        $("#confirm-psw").addClass("has-success");
+        $("#confirm-psw").removeClass("has-error");
+    } 
+    if(confirmpsw != psw) {
+        $("#confirm-psw").addClass("has-error");
+        $("#confirmpsw").removeClass("has-success");
+    }
+});
+});
+
+function formCheck() {
+    var psw = $("#psw");
+    var confirmpsw = $("#confirm-psw");
+    var user = $("#user");
+    
+    if(psw.hasClass("has-success") && confirmpsw.hasClass("has-success") && user.hasClass("has-success")) {
+        return true;
+    } else {
+        alert("Please double check your submitted info.");
+        return false;
+    } 
+}
